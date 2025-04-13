77154944fbc3ae2c14aa84498a557565d32f0be8
KARTIKPAUL <arpilar401@gmail.com>
2025-04-13 14:56:23 +0530

    sicial media add

--- src/scenes/FindUs/Findus.tsx
+++ src/scenes/FindUs/Findus.tsx
@@ -6,6 +6,7 @@ import {
 
   
   FaFacebook,
+  FaInstagram,
 
 } from "react-icons/fa";
 
@@ -53,7 +54,7 @@ export default function FindUs() {
 
               {/* Social Icons */}
               <div className="flex space-x-4 mt-4 text-gray-500 text-2xl">
-                {/* <a href="https://www.facebook.com/thefitboxapd/" className="hover:text-yellow-500"><FaInstagram /></a> */}
+                <a href="https://www.instagram.com/ganeshgurigoldsgym/?hl=en" className="hover:text-yellow-500"><FaInstagram /></a>
                 <a href="https://www.facebook.com/ganeshgurigoldsgym/" className="hover:text-[#2f2484]"><FaFacebook /></a>
                 {/* <a href="https://www.linkedin.com/company/vajjiram" className="hover:text-[#2f2484]"><FaLinkedin /></a> */}
               </div>
